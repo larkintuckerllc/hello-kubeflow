bc76a209ba3ba1ee0990e8517faf52a35398821d
John Tucker <jtucker@spotify.com>
2025-08-03 22:45:47 +0100

    modules

--- hello.py
+++ hello.py
@@ -1,6 +1,9 @@
 from pyspark.sql import SparkSession
 
+from mymodule import beep
 
 spark = SparkSession.builder.appName("Hello World").getOrCreate()
 rdd = spark.sparkContext.parallelize([1, 2, 3, 4, 5])
 print("RDD count: ", rdd.count())
+message = beep()
+print(message)
---
+++ mymodule.py
@@ -0,0 +1,2 @@
+def beep():
+    return "Beep!"
