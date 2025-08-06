02cc173a3f9068ab787914e89a378d04d9f6fa39
John Tucker <jtucker@spotify.com>
2025-08-06 17:05:09 +0100

    checkpoint

--- fare-prediction.py
+++ fare-prediction.py
@@ -1,4 +1,15 @@
+import boto3
+import os
 from pyspark.sql import SparkSession
+
+
+S3_CLIENT = boto3.client("s3")
+BUCKET_NAME = "hello-kubeflow"
+S3_KEY = "fare-prediction/chicago_taxi_train.csv"
+LOCAL_FILE_PATH = "/tmp/data/chicago_taxi_train.csv"
+
+os.makedirs("/tmp/data", exist_ok=True)
+S3_CLIENT.download_file(BUCKET_NAME, S3_KEY, LOCAL_FILE_PATH)
 
 spark = SparkSession.builder.appName("Fare Prediction").getOrCreate()
 df = spark.read.csv("data/chicago_taxi_train.csv", header=True, inferSchema=True)
