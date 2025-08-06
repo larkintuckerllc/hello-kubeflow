f0935570817252f8448b4b7e3b64132e6bcfca70
John Tucker <jtucker@spotify.com>
2025-08-06 15:31:22 -0400

    revert

--- fare-prediction.py
+++ fare-prediction.py
@@ -1,17 +1,7 @@
-import boto3
 import os
 from pyspark.sql import SparkSession
 
-
-S3_CLIENT = boto3.client("s3")
-BUCKET_NAME = "hello-kubeflow"
-S3_KEY = "fare-prediction/chicago_taxi_train.csv"
-LOCAL_FILE_PATH = "/tmp/data/chicago_taxi_train.csv"
-
-os.makedirs("/tmp/data", exist_ok=True)
-S3_CLIENT.download_file(BUCKET_NAME, S3_KEY, LOCAL_FILE_PATH)
-
 spark = SparkSession.builder.appName("Fare Prediction").getOrCreate()
-df = spark.read.csv("data/chicago_taxi_train.csv", header=True, inferSchema=True)
-df.printSchema()
-df.show(5)
+# df = spark.read.csv("data/chicago_taxi_train.csv", header=True, inferSchema=True)
+# df.printSchema()
+# df.show(5)
