d1ce83054db19273af6dcc6c30743f71ff13531a
John Tucker <jtucker@spotify.com>
2025-08-06 15:53:49 -0400

    fix

--- fare-prediction.py
+++ fare-prediction.py
@@ -1,7 +1,3 @@
-import os
 from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName("Fare Prediction").getOrCreate()
-# df = spark.read.csv("data/chicago_taxi_train.csv", header=True, inferSchema=True)
-# df.printSchema()
-# df.show(5)
