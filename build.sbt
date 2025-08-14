f81db3adec059f9715f6ea0ed41a19cfae3f9d03
John Tucker <jtucker@spotify.com>
2025-08-14 19:53:08 -0400

    sparkapplication

--- build.sbt
+++ build.sbt
@@ -2,5 +2,8 @@ ThisBuild / scalaVersion := "2.13.16"
 
 lazy val farePrediction = (project in file("."))
   .settings(
-    name := "Fare Prediction"
+    name := "Fare Prediction",
+    libraryDependencies ++= Seq(
+      "org.apache.spark" %% "spark-sql" % "4.0.0"
+    )
   )
--- src/main/scala/FarePrediction.scala
+++ src/main/scala/FarePrediction.scala
@@ -1,5 +1,10 @@
+import org.apache.spark.sql.SparkSession
+
 object FarePrediction {
   def main(args: Array[String]): Unit = {
-    println("Hello, World!")
+    val spark = SparkSession.builder.appName("Fare Prediction").getOrCreate()
+    val rdd = spark.sparkContext.parallelize(Array(1, 2, 3, 4, 5))
+    println(s"RDD count: ${rdd.count()}")
+    spark.stop()
   }
 }
