a732cd83be9b5f570d63bc52dc839776153bbf0c
John Tucker <jtucker@spotify.com>
2025-08-15 13:01:56 -0400

    checkpoint

--- src/main/scala/FarePrediction.scala
+++ src/main/scala/FarePrediction.scala
@@ -4,10 +4,10 @@ object FarePrediction {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder()
       .appName("Fare Prediction")
-      .config("spark.hadoop.fs.s3a.access.key", "REPLACE") // TODO
-      .config("spark.hadoop.fs.s3a.secret.key", "REPLACE") // TODO
-      .config("spark.hadoop.fs.s3a.session.token", "REPLACE") // TODO
-      .config("spark.hadoop.fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.TemporaryAWSCredentialsProvider") // TODO
+      .config("spark.hadoop.fs.s3a.access.key", sys.env.getOrElse("AWS_ACCESS_KEY_ID", sys.error("AWS_ACCESS_KEY_ID environment variable is required")))
+      .config("spark.hadoop.fs.s3a.secret.key", sys.env.getOrElse("AWS_SECRET_ACCESS_KEY", sys.error("AWS_SECRET_ACCESS_KEY environment variable is required")))
+      .config("spark.hadoop.fs.s3a.session.token", sys.env.getOrElse("AWS_SESSION_TOKEN", sys.error("AWS_SESSION_TOKEN environment variable is required")))
+      .config("spark.hadoop.fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.TemporaryAWSCredentialsProvider") // USING TEMPORARY CREDENTIALS
       .getOrCreate()
     val df = spark.read
       .option("header", "true")
