9ceea616e517bace9206275f0da28c0d49369166
John Tucker <jtucker@spotify.com>
2025-08-15 10:40:29 -0400

    checkpoint

--- src/main/scala/FarePrediction.scala
+++ src/main/scala/FarePrediction.scala
@@ -2,9 +2,15 @@ import org.apache.spark.sql.SparkSession
 
 object FarePrediction {
   def main(args: Array[String]): Unit = {
-    val spark = SparkSession.builder.appName("Fare Prediction").getOrCreate()
-    val rdd = spark.sparkContext.parallelize(Array(1, 2, 3, 4, 5))
-    println(s"RDD count: ${rdd.count()}")
+    val spark = SparkSession.builder()
+      .appName("Fare Prediction")
+      .config("spark.hadoop.fs.s3a.access.key", "REPLACE") // Replace with your key
+      .config("spark.hadoop.fs.s3a.secret.key", "REPLACE") // Replace with your key
+      .getOrCreate()
+    val df = spark.read
+      .option("header", "true")
+      .csv("s3a://hello-kubeflow/fare-prediction/chicago_taxi_train.csv")
+    df.show()
     spark.stop()
   }
 }
