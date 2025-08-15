09ff9fb91f9ed4a74106367a4d9a1bb54b192e86
John Tucker <jtucker@spotify.com>
2025-08-15 12:53:31 -0400

    checkpoint

--- build.sbt
+++ build.sbt
@@ -5,7 +5,5 @@ lazy val farePrediction = (project in file("."))
     name := "Fare Prediction",
     libraryDependencies ++= Seq(
       "org.apache.spark" %% "spark-sql" % "4.0.0",
-      "org.apache.hadoop" % "hadoop-aws" % "3.4.0",
-      "org.apache.hadoop" % "hadoop-client" % "3.4.0"
     )
   )
--- src/main/scala/FarePrediction.scala
+++ src/main/scala/FarePrediction.scala
@@ -4,15 +4,10 @@ object FarePrediction {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder()
       .appName("Fare Prediction")
-      .config("spark.hadoop.fs.s3a.access.key", "REPLACE") // Replace with your key
-      .config("spark.hadoop.fs.s3a.secret.key", "REPLACE") // Replace with your key
+      .config("spark.hadoop.fs.s3a.access.key", "REPLACE") // TODO
+      .config("spark.hadoop.fs.s3a.secret.key", "REPLACE") // TODO
       .config("spark.hadoop.fs.s3a.session.token", "REPLACE") // TODO
-      .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
-      .config("spark.hadoop.fs.s3a.endpoint", "s3.amazonaws.com")
-      .config("spark.hadoop.fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.TemporaryAWSCredentialsProvider")
-      .config("spark.hadoop.fs.s3a.connection.maximum", "1000")
-      .config("spark.hadoop.fs.s3a.connection.timeout", "200000")
-      .config("spark.hadoop.fs.s3a.connection.ttl", "200000")
+      .config("spark.hadoop.fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.TemporaryAWSCredentialsProvider") // TODO
       .getOrCreate()
     val df = spark.read
       .option("header", "true")
