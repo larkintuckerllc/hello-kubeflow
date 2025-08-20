665e64402ee0ee86890dffb0bb98ff37f11b1981
John Tucker <jtucker@spotify.com>
2025-08-20 08:45:46 -0400

    checkpoint

--- src/main/scala/FarePrediction.scala
+++ src/main/scala/FarePrediction.scala
@@ -1,4 +1,7 @@
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.lit
+import org.apache.spark.sql.functions.when
+import org.apache.spark.sql.functions.rand
 
 object FarePrediction {
   def main(args: Array[String]): Unit = {
@@ -9,10 +12,14 @@ object FarePrediction {
       .config("spark.hadoop.fs.s3a.session.token", sys.env.getOrElse("AWS_SESSION_TOKEN", sys.error("AWS_SESSION_TOKEN environment variable is required")))
       .config("spark.hadoop.fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.TemporaryAWSCredentialsProvider") // USING TEMPORARY CREDENTIALS
       .getOrCreate()
-    val df = spark.read
+    var ds = spark.read
       .option("header", "true")
       .csv("s3a://hello-kubeflow/fare-prediction/chicago_taxi_train.csv")
-    df.show()
+      .limit(1000) // TODO: REMOVE THIS WHEN RUNNING AS A SPARKAPPLICATION
+    ds = ds.withColumn("TRIP_MINUTES", ds("TRIP_SECONDS") / 60)
+    ds = ds.withColumn("PARTITION", when(rand() < 0.6, lit("train")).otherwise(when(rand() < 0.5, lit("validation")).otherwise(lit("test"))))
+    ds = ds.select("FARE", "TRIP_MILES", "TRIP_MINUTES", "PARTITION")
+    ds.show()
     spark.stop()
   }
 }
