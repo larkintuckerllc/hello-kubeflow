13a7fbead70a12720c8043950ccbca1953808a8b
John Tucker <jtucker@spotify.com>
2025-08-29 15:26:59 -0400

    reorg training

--- notebooks/00-linear-regression.py
+++ notebooks/00-linear-regression.py
@@ -23,7 +23,7 @@ from kubeflow.trainer import TrainerClient, CustomTrainer
 
 
 # %% [markdown]
-# # train_pytorch
+# # training function
 
 # %%
 def train_pytorch():
@@ -45,28 +45,11 @@ def train_pytorch():
 
     device, backend = ("cuda", "nccl") if torch.cuda.is_available() else ("cpu", "gloo")
     dist.init_process_group(backend=backend)
+    world_size = dist.get_world_size()
+    rank = dist.get_rank()
     local_rank = int(os.getenv("LOCAL_RANK", 0))
-    print(
-        "Distributed Training with WORLD_SIZE: {}, RANK: {}, LOCAL_RANK: {}.".format(
-            dist.get_world_size(),
-            dist.get_rank(),
-            local_rank,
-        )
-    )
-
-    class NeuralNetwork(torch.nn.Module):
-        def __init__(self):
-            super().__init__()
-            self.linear = torch.nn.Linear(1, 1)
-            
-        def forward(self, x):
-            return self.linear(x)
-
     device = torch.device(f"{device}:{local_rank}")
-    model = nn.parallel.DistributedDataParallel(NeuralNetwork().to(device))
-    model.train()
-    loss_fn = torch.nn.MSELoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=LEARNING_RATE)
+    print(f"Distributed Training with WORLD_SIZE: {world_size}, RANK: {rank}, LOCAL_RANK: {local_rank}.")
 
     class CustomDataset(Dataset):
         def __init__(self, bucket_name, object_path):
@@ -78,10 +61,24 @@ def train_pytorch():
             return self.pounds.shape[0]
         
         def __getitem__(self, idx):
-            return self.pounds[idx].view(-1, 1), self.mpg[idx].view(-1, 1)
-    
+            return self.pounds[idx].view(-1, 1), self.mpg[idx].view(-1, 1) 
+
+    distributed_batch_size = BATCH_SIZE // world_size
     dataset = CustomDataset(BUCKET_NAME, OBJECT_PATH)
-    dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, sampler=DistributedSampler(dataset))
+    dataloader = DataLoader(dataset, batch_size=distributed_batch_size, sampler=DistributedSampler(dataset))
+
+    class NeuralNetwork(torch.nn.Module):
+        def __init__(self):
+            super().__init__()
+            self.linear = torch.nn.Linear(1, 1)
+            
+        def forward(self, x):
+            return self.linear(x)
+
+    model = nn.parallel.DistributedDataParallel(NeuralNetwork().to(device))
+    loss_fn = torch.nn.MSELoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=LEARNING_RATE)
+
     for epoch in range(EPOCHS):
         total_loss = 0
         for batch_pounds, batch_mpg in dataloader:
@@ -92,24 +89,23 @@ def train_pytorch():
             optimizer.step()
             optimizer.zero_grad()
             total_loss += loss.item()
-        if (epoch + 1) % 10 == 0 and dist.get_rank() == 0:
+        if (epoch + 1) % 10 == 0 and rank == 0:
             avg_loss = total_loss / len(dataloader)
             print(f'Epoch [{epoch+1}/{EPOCHS}], Average Loss: {avg_loss:.4f}')
     dist.barrier()
-    if dist.get_rank() == 0:
+    if rank == 0:
         print("Training is finished")
     dist.destroy_process_group()
 
-
 # %% [markdown]
-# # Validate Runtime
+# # validate runtime
 
 # %%
 for r in TrainerClient().list_runtimes():
     print(f"Runtime: {r.name}")
 
 # %% [markdown]
-# # Create Training Job
+# # create training job
 
 # %%
 job_id = TrainerClient().train(
@@ -126,7 +122,7 @@ job_id = TrainerClient().train(
             "pandas==2.3.2",
         ],
         resources_per_node={
-            "cpu": 5,
+            "cpu": 2,
             "memory": "4Gi",
             # "gpu": 1, # Comment this line if you don't have GPUs.
         },
@@ -135,7 +131,7 @@ job_id = TrainerClient().train(
 )
 
 # %% [markdown]
-# # TODO
+# # training job status
 
 # %%
 for s in TrainerClient().get_job(name=job_id).steps:
@@ -143,7 +139,7 @@ for s in TrainerClient().get_job(name=job_id).steps:
 
 
 # %% [markdown]
-# # TODO
+# # training job output
 
 # %%
 logs = TrainerClient().get_job_logs(name=job_id)
